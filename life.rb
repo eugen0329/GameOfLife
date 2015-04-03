e8dab2888fad3aa8a0eef01d763a8fb9cad1c3fe
eugen0329 <eugen0329@gmail.com>
2015-04-03 14:41:28 +0000

    Added animation refresh rate customization via cmd line

--- life.rb
+++ life.rb
@@ -3,11 +3,13 @@
 require  'terminfo'
 
 SELF_DIR = File.expand_path('../', __FILE__)
+DEFAULT_REFRESH_RATE = 0.11
 Dir["#{SELF_DIR}/lib/*.rb"].each { |lib| require_relative lib }
 
 size = Lifegame::Coordinates.new(*TermInfo.screen_size)
 size.y -= 1         # to prevent wrong displaying when tmux panel isn't hidden
-refresh_rate = 0.05
+
+refresh_rate = ARGV.empty? ? DEFAULT_REFRESH_RATE : ARGV.shift.to_f
 
 m = Lifegame::Map.new(size.y, size.x)
 
